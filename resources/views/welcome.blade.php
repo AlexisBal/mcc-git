7d1def364e5080eeffbdf744a553b250aae5b130
mycoursechoice <60859013+mycoursechoice@users.noreply.github.com>
2020-03-14 01:08:30 +0100

    enter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
     <div class="section">
         <h1 class="title is-1">Bienvenue sur My Course Choice !</h1>
         <h2 class="title is-2">L'outil qui te permet de trouver l'école de tes rêves.</h2>
+        <h3 class="title is-3">Mais comment cela fonctionne ?</h3>
 
-        <p>bonjour</p>
+        <p>Tout d’abord, tu dois t’inscrire. Attention, lors de l’inscription, des questions personnelles te seront posées. Tu dois y répondre en toute honnêteté, puisqu’il n’y a pas de bonne ou mauvaise réponse.   
+Puis, notre algorithme étudie ta personnalité afin de déterminer la formation qui te correspond le plus. C’est pourquoi, il est nécessaire de prendre son temps lors de l’inscription afin de décrire le plus précisément ta personnalité et obtenir les propositions qui te correspondent le plus.  </p>
 @endsection
 

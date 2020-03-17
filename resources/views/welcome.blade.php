e8dee3458c0dfc2be9dcec4a7771ba501c85cda8
mycoursechoice <60859013+mycoursechoice@users.noreply.github.com>
2020-03-17 19:35:06 +0100

    enter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
         <h2 class="title is-2">L'outil qui te permet de trouver l'école de tes rêves.</h2>
         <h3 class="title is-3">Mais comment cela fonctionne ?</h3>
 
-        <p>Tout d’abord, tu dois t’inscrire. Attention, lors de l’inscription, des questions personnelles te seront posées. Tu dois y répondre en toute honnêteté, puisqu’il n’y a pas de bonne ou mauvaise réponse.   
-Puis, notre algorithme étudie ta personnalité afin de déterminer la formation qui te correspond le plus. C’est pourquoi, il est nécessaire de prendre son temps lors de l’inscription afin de décrire le plus précisément ta personnalité et obtenir les propositions qui te correspondent le plus.  </p>
+    
 @endsection
 

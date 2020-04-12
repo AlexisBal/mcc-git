a0e23e0133f105acec90e5c78292c81bda8b0006
mycoursechoice <60859013+mycoursechoice@users.noreply.github.com>
2020-04-12 14:48:29 +0200

    mcc
    Version transition Bulma -> Bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,62 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>My Course Choice - @yield('title')</title>
+    <title>My Course Choice - @yield('title')</title>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-    </head>
-    <body>
-        <nav class="navbar is-light">
-          <div class="navbar-menu">
-            <div class="navbar-start">
-                @auth
-                    <a href="/mon-compte" class="navbar-item">Accueil</a>   
-                @else            
-                    <a href="/" class="navbar-item">Accueil</a>
-                @endauth                
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        @auth
+                            <a href="/mon-compte" class="nav-link">Accueil</a>   
+                        @else            
+                            <a href="/" class="nav-link">Accueil</a>
+                        @endauth  
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        @auth
+                            <a href="/profil" class="nav-link">Mon profil</a>            
+                            <a href="/deconnexion" class="nav-link">Déconnexion</a>
+                        @else
+                            <a href="/connexion" class="nav-link">Connexion</a>                  
+                            <a href="/inscription" class="nav-link">Inscription</a>
+                        @endauth
+                    </ul>
+                </div>
             </div>
-            <div class="navbar-end"> 
-                @auth
-                    <a href="/profil" class="navbar-item {{ request()->is('profil') ? 'is-active' : '' }}">Mon profil</a>            
-                    <a href="/deconnexion" class="navbar-item">Déconnexion</a>
-                @else
-                    <a href="/connexion" class="navbar-item {{ request()->is('connexion') ? 'is-active' : '' }}">Connexion</a>                  
-                    <a href="/inscription" class="navbar-item {{ request()->is('inscription') ? 'is-active' : '' }}">Inscription</a>
-                @endauth  
-            </div>
-          </div>
         </nav>
 
-
-        <div class="container">
+        <main class="py-4">
             @include('flash::message')
 
             @yield('content')
-        </div>
-    </body>
+        </main>
+    </div>
+</body>
 </html>

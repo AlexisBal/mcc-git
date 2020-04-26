8dc5d4281d2773c516c0b5537b79d31ddca1a563
mycoursechoice <60859013+mycoursechoice@users.noreply.github.com>
2020-04-27 01:58:20 +0200

    vérification-pas-de-qualite-defaut-en-double

--- app/Http/Controllers/InformationsProfilController.php
+++ app/Http/Controllers/InformationsProfilController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Utilisateur;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class InformationsProfilController extends Controller
 {
@@ -69,18 +70,19 @@ class InformationsProfilController extends Controller
 
 	public function traitement()
     {   
-        request()->validate([
+        request()->validate([ 
 			'niveauetude' => ['required', 'string', 'max:255'],
 			'specialite' => ['required', 'string', 'max:255'],
-			'qualite1' => ['required', 'string', 'max:255'],
-			'qualite2' => ['required', 'string', 'max:255'],
-			'qualite3' => ['required', 'string', 'max:255'],
-			'qualite4' => ['required', 'string', 'max:255'],
-			'qualite5' => ['required', 'string', 'max:255'],
-			'defaut1' => ['required', 'string', 'max:255'],
-			'defaut2' => ['required', 'string', 'max:255'],
-			'defaut3' => ['required', 'string', 'max:255'],
+			'qualite1' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite5'],
+			'qualite2' => ['required', 'string', 'max:255', 'different:qualite1','different:qualite3', 'different:qualite4', 'different:qualite5'],
+			'qualite3' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite1', 'different:qualite4', 'different:qualite5'],
+            'qualite4' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite1', 'different:qualite5'],
+			'qualite5' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite1'],
+			'defaut1' => ['required', 'string', 'max:255', 'different:defaut2','different:defaut3'],
+			'defaut2' => ['required', 'string', 'max:255', 'different:defaut3','different:defaut1'],
+			'defaut3' => ['required', 'string', 'max:255', 'different:defaut1','different:defaut2'],
         ]);
+        
         $utilisateurs = Utilisateur::where('id', '=',Auth::user()->id)->first();
         $id = $utilisateurs->id;
 

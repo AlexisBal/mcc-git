5a9edcc61bfc97b03d72916320fa702f519c6083
mycoursechoice <60859013+mycoursechoice@users.noreply.github.com>
2020-05-03 02:17:09 +0200

    formulaire profil
    Partie Back + début Front

--- app/Http/Controllers/InformationsProfilController.php
+++ app/Http/Controllers/InformationsProfilController.php
@@ -11,7 +11,7 @@ use Illuminate\Validation\Rule;
 
 class InformationsProfilController extends Controller
 {
-    public function formulaire()
+    public function formulaire_part1()
     {   
         $spe = request()->old('specialite');
 
@@ -66,21 +66,37 @@ class InformationsProfilController extends Controller
 		return view('mes-informations',[
             'specialite' => $spe,
         ]);
-	}
+    }
+    
+    public function formulaire_part2()
+    {   
+		return view('mes-informations-2');
+    }
 
-	public function traitement()
+    public function formulaire_part3()
+    {   
+		return view('mes-informations-3');
+    }
+    
+    public function traitement_part1()
     {   
         request()->validate([ 
 			'niveauetude' => ['required', 'string', 'max:255'],
 			'specialite' => ['required', 'string', 'max:255'],
-			'qualite1' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite5'],
-			'qualite2' => ['required', 'string', 'max:255', 'different:qualite1','different:qualite3', 'different:qualite4', 'different:qualite5'],
-			'qualite3' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite1', 'different:qualite4', 'different:qualite5'],
-            'qualite4' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite1', 'different:qualite5'],
-			'qualite5' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite1'],
-			'defaut1' => ['required', 'string', 'max:255', 'different:defaut2','different:defaut3'],
-			'defaut2' => ['required', 'string', 'max:255', 'different:defaut3','different:defaut1'],
-			'defaut3' => ['required', 'string', 'max:255', 'different:defaut1','different:defaut2'],
+            'lv1' => ['required', 'string', 'max:255'],
+            'profileleve' => ['required', 'string', 'max:255'],
+            'ambiancetravail' => ['required', 'string', 'max:255'],
+            'tempstravail' => ['required', 'string', 'max:255'],
+            'methodes' => ['required', 'string', 'max:255'],
+            'matieresfav' => ['required', 'string', 'max:255'],
+            'maths' => ['required', 'string', 'max:255'],
+            'physique' => ['required', 'string', 'max:255'],
+            'svt' => ['required', 'string', 'max:255'],
+            'si' => ['required', 'string', 'max:255'],
+            'eco' => ['required', 'string', 'max:255'],
+            'francais' => ['required', 'string', 'max:255'],
+            'philosophie' => ['required', 'string', 'max:255'], 
+            'anglais' => ['required', 'string', 'max:255'],
         ]);
         
         $utilisateurs = Utilisateur::where('id', '=',Auth::user()->id)->first();
@@ -90,6 +106,46 @@ class InformationsProfilController extends Controller
             'user_id' => $id,
             'niveauetude' => request('niveauetude'),
             'specialite' => request('specialite'),
+            'lv1' => request('lv1'),
+            'profileleve' => request('profileleve'),
+            'ambiancetravail' => request('ambiancetravail'),
+            'tempstravail' => request('tempstravail'),
+            'methodes' => request('methodes'),
+            'matieresfav' => request('matieresfav'),
+            'maths' => request('maths'),
+            'physique' => request('physique'),
+            'svt' => request('svt'),
+            'si' => request('si'),
+            'eco' => request('eco'),
+            'francais' => request('francais'),
+            'philosophie' => request('philosophie'),
+            'anglais' => request('anglais'),
+        ]);
+
+        {
+            return redirect('/mes-informations-suite');
+        }
+    }
+    
+	public function traitement_part2()
+    {   
+        request()->validate([ 
+			'qualite1' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite5'],
+			'qualite2' => ['required', 'string', 'max:255', 'different:qualite1','different:qualite3', 'different:qualite4', 'different:qualite5'],
+			'qualite3' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite1', 'different:qualite4', 'different:qualite5'],
+            'qualite4' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite1', 'different:qualite5'],
+			'qualite5' => ['required', 'string', 'max:255', 'different:qualite2','different:qualite3', 'different:qualite4', 'different:qualite1'],
+			'defaut1' => ['required', 'string', 'max:255', 'different:defaut2','different:defaut3'],
+			'defaut2' => ['required', 'string', 'max:255', 'different:defaut3','different:defaut1'],
+            'defaut3' => ['required', 'string', 'max:255', 'different:defaut1','different:defaut2'],
+            'raison' => ['required', 'string', 'max:255'],
+            'detail' => ['required', 'string', 'max:255'],
+            'hobbies' => ['required', 'string', 'max:255'],
+            'domaines' => ['required', 'string', 'max:255'],
+            'domainespe' => ['required', 'string', 'max:255'],
+        ]);
+        
+        $profil = Profil::where('user_id', '=',Auth::user()->id)->create([
             'qualite1' => request('qualite1'),
             'qualite2' => request('qualite2'),
             'qualite3' => request('qualite3'),
@@ -98,6 +154,49 @@ class InformationsProfilController extends Controller
             'defaut1' => request('defaut1'),
             'defaut2' => request('defaut2'),
             'defaut3' => request('defaut3'),
+            'raison' => request('raison'),
+            'detail' => request('detail'),
+            'hobbies' => request('hobbies'),
+            'domaines' => request('domaines'),
+            'domainespe' => request('domainespe'),
+        ]);
+
+        {
+
+            return redirect('/mes-informations-fin');
+        }
+    }
+    
+    public function traitement_part3()
+    {   
+        request()->validate([ 
+			'metiers' => ['required', 'string', 'max:255'], 
+            'metierspe' => ['required', 'string', 'max:255'],
+            'situation' => ['required', 'string', 'max:255'],
+            'salaire' => ['required', 'string', 'max:255'],
+            'projet' => ['required', 'string', 'max:255'],
+            'equipe' => ['required', 'string', 'max:255'],
+            'stress' => ['required', 'string', 'max:255'],
+            'international' => ['required', 'string', 'max:255'],
+            'admission' => ['required', 'string', 'max:255'],
+            'prix' => ['required', 'string', 'max:255'],
+            'formations' => ['required', 'string', 'max:255'],
+            'formationspe' => ['required', 'string', 'max:255'],
+        ]);
+        
+        $profil = Profil::where('user_id', '=',Auth::user()->id)->create([
+            'metiers' => request('metiers'),
+            'metierspe' => request('metierspe'),
+            'situation' => request('situation'),
+            'salaire' => request('salaire'),
+            'projet' => request('projet'),
+            'equipe' => request('equipe'),
+            'stress' => request('stress'),
+            'international' => request('international'),
+            'admission' => request('admission'),
+            'prix' => request('prix'),
+            'formations' => request('formations'),
+            'formationspe' => request('formationspe'),
         ]);
 
         {
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,12 @@ Route::group([
     'middleware' => 'App\Http\Middleware\Auth',
 ], function () {
     Route::get('/deconnexion', 'CompteController@deconnexion');
-    Route::get('/mes-informations', 'InformationsProfilController@formulaire');
-    Route::post('/mes-informations', 'InformationsProfilController@traitement');
+    Route::get('/mes-informations', 'InformationsProfilController@formulaire_part1');
+    Route::post('/mes-informations', 'InformationsProfilController@traitement_part1');
+    Route::get('/mes-informations-suite', 'InformationsProfilController@formulaire_part2');
+    Route::post('/mes-informations-suite', 'InformationsProfilController@traitement_part2');
+    Route::get('/mes-informations-fin', 'InformationsProfilController@formulaire_part3');
+    Route::post('/mes-informations-fin', 'InformationsProfilController@traitement_part3');
 });
 
 Route::group([

21db02fb319c920b9b2051a1cb0154cda65262bb
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-05-11 18:58:42 +0200

    J'ai galeré sa mère

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,13 +94,13 @@
 
         <!--Mask-->
         <div class="jumbotron big-banner" style="height: 1000px; padding-top: 150px">
-            <div class="jumbotron">
+            
                 <h1 class="display-4">Bienvenue sur My Course Choice !</h1>
                 <p class="lead">L'outil qui te permet de trouver l'école de tes rêves.</p>
                 <hr class="my-4">
                 <p>Mais comment cela fonctionne ?</p>
                 <a class="btn btn-primary btn-lg" href="#" role="button">En apprendre plus</a>
-            </div>
+          
 
         </div>
 
